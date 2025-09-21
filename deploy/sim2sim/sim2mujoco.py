f1582a53c9c683b2acbcc6c97e979ee1b65d7e92
hyyyyyyz <1783866380@qq.com>
2025-09-21 18:32:55 +0800

    update sim2sim

--- deploy/sim2sim/sim2mujoco.py
+++ deploy/sim2sim/sim2mujoco.py
@@ -114,29 +114,22 @@ def run_mujoco(policy, cfg):
     for _ in tqdm(range(int(cfg.sim_config.sim_duration / cfg.sim_config.dt)), desc="Simulating..."):
 
         # Obtain an observation
-        q, dq, quat, v, omega, gvec = get_obs(data)#从mujoco获取仿真数据
+        q, dq, quat, v, omega, gvec = get_obs(data) # 从mujoco获取仿真数据
         q = q[-cfg.env.num_actions:]
         dq = dq[-cfg.env.num_actions:]
-        
-        # obs_buf =torch.cat((self.base_ang_vel  * self.obs_scales.ang_vel,
-        #                     self.base_euler_xyz * self.obs_scales.quat,
-        #                     self.commands[:, :3] * self.commands_scale,#xy+航向角速度
-        #                     self.reindex((self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos),
-        #                     self.reindex(self.dof_vel * self.obs_scales.dof_vel),
-        #                     self.action_history_buf[:,-1]),dim=-1)#列表最后一项 [:-1]也就是上一次的
 
         if 1:
             # 1000hz ->50hz
             if count_lowlevel % cfg.sim_config.decimation == 0:
 
-                obs = np.zeros([1, cfg.env.n_proprio], dtype=np.float32) #1,45
+                obs = np.zeros([1, cfg.env.n_proprio], dtype = np.float32)    # 1,45
 
                 eu_ang = quaternion_to_euler_array(quat)
                 eu_ang[eu_ang > math.pi] -= 2 * math.pi
 
-                cmd.vx=0.25
-                cmd.vy=0.0
-                cmd.dyaw= 0.3
+                cmd.vx = 0.25
+                cmd.vy = 0.0
+                cmd.dyaw = 0.3
                 #sensor->lcm
                 #单次观测
                 obs[0, 0] = omega[0] *cfg.normalization.obs_scales.ang_vel
@@ -148,51 +141,40 @@ def run_mujoco(policy, cfg):
                 obs[0, 6] = cmd.vx * cfg.normalization.obs_scales.lin_vel
                 obs[0, 7] = cmd.vy * cfg.normalization.obs_scales.lin_vel
                 obs[0, 8] = cmd.dyaw * cfg.normalization.obs_scales.ang_vel
-                obs[0, 9:21] = (q-default_dof_pos) * cfg.normalization.obs_scales.dof_pos #g关节角度顺序依据修改为样机
+                obs[0, 9:21] = (q-default_dof_pos) * cfg.normalization.obs_scales.dof_pos # g关节角度顺序依据修改为样机
                 obs[0, 21:33] = dq * cfg.normalization.obs_scales.dof_vel
-                obs[0, 33:45] = last_actions#上次控制指令
+                obs[0, 33:45] = last_actions # 上次控制指令
                 obs = np.clip(obs, -cfg.normalization.clip_observations, cfg.normalization.clip_observations)
 
-                # obs_cpu = obs  # 首先将Tensor移动到CPU，然后转换为NumPy数组 
-                # for i in range(3):
-                #     print("{:.2f}".format(obs_cpu[0][i]))
-                # for i in range(3):  
-                #     print("{:.2f}".format(obs_cpu[0][i+3]))
-
-                hist_obs.append(obs) #11,1,45
-                hist_obs.popleft() #10,1,45
+                hist_obs.append(obs) # 11,1,45
+                hist_obs.popleft() # 10,1,45
 
                 n_proprio=cfg.env.n_proprio
                 n_priv_latent=cfg.env.n_priv_latent
                 n_scan=cfg.env.n_scan
                 history_len=cfg.env.history_len
-                num_observations= cfg.env.num_observations# cfg.env.n_proprio + cfg.env.history_len*cfg.env.n_proprio
+                num_observations= cfg.env.num_observations # cfg.env.n_proprio + cfg.env.history_len*cfg.env.n_proprio
 
-                policy_input = np.zeros([1, num_observations], dtype=np.float16) # 同isaac 完整
-                hist_obs_input = np.zeros([1, history_len*n_proprio], dtype=np.float16) # 同isaac 观测buf
-                #依据完成模型的顺序实际只采用了前后的观测数据
+                policy_input = np.zeros([1, num_observations], dtype=np.float16) # 同 isaac 完整
+                hist_obs_input = np.zeros([1, history_len*n_proprio], dtype=np.float16) # 同 isaac 观测buf
+                # 依据完成模型的顺序实际只采用了前后的观测数据
                 policy_input[0,0:n_proprio]=obs
-                for i in range(n_priv_latent  + n_scan):#缓存历史观测
+                for i in range(n_priv_latent  + n_scan):    # 缓存历史观测
                     policy_input[0,n_proprio+i]=0
-                for i in range(history_len):#缓存历史观测
+                for i in range(history_len):    # 缓存历史观测
                     policy_input[0, n_proprio  + n_priv_latent  + n_scan +i * n_proprio : n_proprio  + n_priv_latent  + n_scan +(i + 1) * n_proprio] = hist_obs[i][0, :]
-                #采集模型仅仅使用观测和buf
-                for i in range(history_len):#缓存历史观测
+                # 采集模型仅仅使用观测和buf
+                for i in range(history_len):    # 缓存历史观测
                     hist_obs_input[0, i * n_proprio : (i + 1) * n_proprio] = hist_obs[i][0, :]
                
                 policy = policy.to('cpu') #policy是half()
-                action[:] = policy.act_teacher(torch.tensor(policy_input).half())[0].detach().numpy()#完整模型
-                #action[:] = policy(torch.tensor(obs).half(),torch.tensor(hist_obs_input).half())[0].detach().numpy()#jit模型
+                action[:] = policy.act_teacher(torch.tensor(policy_input).half())[0].detach().numpy()   # 完整模型
   
                 action = np.clip(action, -cfg.normalization.clip_actions, cfg.normalization.clip_actions)
                 action_flt=_low_pass_action_filter(action,last_actions)
                 last_actions=action
 
                 target_q = action_flt * cfg.control.action_scale + default_dof_pos
-                # lcm->mujoco
-                # print("action_flt:")
-                # print(action_flt)
-                # print(target_q)
 
             target_dq = np.zeros((cfg.env.num_actions), dtype=np.double)
             # Generate PD control
@@ -200,19 +182,13 @@ def run_mujoco(policy, cfg):
                              target_dq, dq, cfg.robot_config.kds)  # Calc torques
             tau = np.clip(tau, -cfg.robot_config.tau_limit, cfg.robot_config.tau_limit)  # Clamp torques
             data.ctrl = tau
-        else:#air mode test
+        else:   # air mode test
             obs = np.zeros([1, cfg.env.n_proprio], dtype=np.float32) #1,45
             eu_ang = quaternion_to_euler_array(quat)
             eu_ang[eu_ang > math.pi] -= 2 * math.pi
             target_q = default_dof_pos
-            # target_q[0]=0
-            # target_q[1]=3
-            # target_q[2]=3
-            # target_q[3]=0
-            # target_q[4]=3
-            # target_q[5]=3     
-            #print(eu_ang*57.3)
             target_dq = np.zeros((cfg.env.num_actions), dtype=np.double)
+
             # Generate PD control
             tau = pd_control(target_q, q, cfg.robot_config.kps,
                             target_dq, dq, cfg.robot_config.kds)  # Calc torques
